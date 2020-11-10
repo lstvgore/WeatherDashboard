be1da45093b61d8d0400fa41c58d751e50d15426
stvgo15 <stv.gore@gmail.com>
2020-11-10 14:26:00 -0800

    weather homework

--- Assets/app.js
+++ Assets/app.js
@@ -13,7 +13,7 @@ button.addEventListener('click', function (event) {
     event.preventDefault()
     var inputValue = document.querySelector('.Submit');
     var city = inputValue.value
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=1110a537a5d0dc6edde7bf296b840bde')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=1110a537a5d0dc6edde7bf296b840bde')
         .then(Response => Response.json())
         .then(data => {
             console.log(data)
@@ -21,7 +21,7 @@ button.addEventListener('click', function (event) {
       
             var inputValue = document.querySelector('.Submit');
             var city = inputValue.value
-            fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=1110a537a5d0dc6edde7bf296b840bde')
+            fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=1110a537a5d0dc6edde7bf296b840bde')
                 .then(data => {
                     console.log(data) })
 
@@ -31,7 +31,7 @@ button.addEventListener('click', function (event) {
                     var tempValue = data.main.temp;
                     var descValue = data.weather[0].description;
                     var iconValue = data.weather[0].icon;
-                    var iconurl = 'http://openweathermap.org/img/wn/' + iconValue + '@2x.png'
+                    var iconurl = 'https://openweathermap.org/img/wn/' + iconValue + '@2x.png'
                     var cardValue = data.main.temp;
 
 
